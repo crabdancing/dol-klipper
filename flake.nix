89764dbf2ed71707358c1bdf4de2d2ec1854b9ef
Alexandria P. <alxpettit@gmail.com>
2024-05-18 18:14:50 -0600

    klipper for 4.2.7 board

--- flake.nix
+++ flake.nix
@@ -1,8 +1,38 @@
+# {
+#   description = "A flake for building Klipper firmware for various printer boards";
+
+#   inputs = {
+#     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+#     flake-utils.url = "github:numtide/flake-utils";
+#   };
+
+#   outputs = { self, nixpkgs, flake-utils }:
+#     flake-utils.lib.eachDefaultSystem (system:
+#       let
+#         pkgs = import nixpkgs {
+#           inherit system;
+#         };
+#       in {
+#         packages.klipper-firmware-atmega2560 = pkgs.klipper-firmware.override {
+#           mcu = "atmega2560";
+#           firmwareConfig = ./config/atmega2560.cfg;
+#         };
+
+#         packages.klipper-firmware-stm32f103 = pkgs.klipper-firmware.override {
+#           mcu = "stm32f103";
+#           firmwareConfig = ./config/stm32f103.cfg;
+#         };
+
+#         # Add more targets as needed...
+#       }
+#     );
+# }
+
 {
   description = "A flake for building Klipper firmware for various printer boards";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -12,18 +42,39 @@
         pkgs = import nixpkgs {
           inherit system;
         };
+        klipperVersion = pkgs.klipper.version;
       in {
-        packages.klipper-firmware-atmega2560 = pkgs.klipper-firmware.override {
-          mcu = "atmega2560";
-          firmwareConfig = ./config/atmega2560.cfg;
+        packages = {
+          klipper-firmware-atmega2560 = pkgs.klipper-firmware.override {
+            mcu = "atmega2560";
+            firmwareConfig = ./config/atmega2560.cfg;
+          };
+
+          "klipper-firmware-ender3v2-board_4_2_7" = pkgs.klipper-firmware.override {
+            mcu = "stm32f103";
+            firmwareConfig = ./firmware-config/ender3v2-board4.2.7;
+          };
+
+          # Add more targets as needed...
         };
 
-        packages.klipper-firmware-stm32f103 = pkgs.klipper-firmware.override {
-          mcu = "stm32f103";
-          firmwareConfig = ./config/stm32f103.cfg;
-        };
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            (pkgs.klipper.overrideAttrs (oldAttrs: {
+              version = klipperVersion;
+            }))
+            (pkgs.klipper-genconf.overrideAttrs (oldAttrs: {
+              version = klipperVersion;
+            }))
+            (pkgs.klipper-flash.overrideAttrs (oldAttrs: {
+              version = klipperVersion;
+            }))
+          ];
 
-        # Add more targets as needed...
+          shellHook = ''
+            echo "Klipper version: ${klipperVersion}"
+          '';
+        };
       }
     );
 }
